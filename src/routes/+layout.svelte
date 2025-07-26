b389909738d0501aa22d838a1772e2a6bbdf8d62
Rudraksh Tripathi <rudraksh.tripathi8@gmail.com>
2025-07-26 21:24:49 +0530

    feat: spring transition for sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,29 +4,30 @@
 
 	let { children } = $props();
 
-	let sidebarState = $state(true);
+	let sidebarState = $state(false);
 	let flexClass = $state('flex-[.2]');
 </script>
 
 <!-- Bouncy Cubic Bezier -->
-<div class="background-container flex min-w-screen min-h-screen bg-neutral-100 *:transition-all  *:ease-cubic-bezier(0.25, 0.1, 0.25, 1) rounded-lg">
+<div class="background-container flex min-w-screen min-h-screen bg-neutral-100 *:transition-all *:ease-cubic-bezier(0.25, 0.1, 0.25, 1) rounded-lg">
 	<!-- svelte-ignore a11y_no_static_element_interactions -->
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
-	<div class="sidebar-container p-2 select-none cursor-pointer group/sidebar {sidebarState ? 'flex-[.2] ease-[cubic-bezier(0.34,1.56,0.64,1)] duration-500' : 'flex-[.028] ease-[cubic-bezier(0.34,1.56,0.64,1)] duration-500'}"
+	<div class="sidebar-container p-2 select-none group/sidebar spring-transition {sidebarState ? 'flex-[.2]' : 'flex-[.028]'}"
 		onclick={() => {
-				// Expand
-				sidebarState = !sidebarState;
-			}}>
-		<Sidebar
-		/>
+			sidebarState = !sidebarState;
+		}}
+	>
+		<Sidebar/>
 	</div>
 	<div class="content-container flex-1 p-2 pl-0">
 		<div class="h-full w-full p-3 px-5 bg-white border-2 border-neutral-300/60 rounded-md shadow-xl shadow-[#000000005]">
 			{@render children()}
 		</div>
 	</div>
-</div>div
+</div>
 
 <style>
-
+.spring-transition {
+	transition: flex 1050ms linear(0, 0.047665487349415225, 0.16760867250925093, 0.32828759624209514, 0.5035255537397554, 0.6733354159847009, 0.8239491027982078, 0.9472773869583244, 1.0400120704591855, 1.1025540666473026, 1.1379140236250675, 1.150692569983367, 1.1462093844070602, 1.1298170242445091, 1.1064084744721436, 1.080107327983694, 1.0541162057053923, 1.0306917368237911, 1.0112120791736428, 0.9963043604466211, 0.986003363657431, 0.9799181807397408, 0.9773895110625852, 0.9776260914878324, 0.9798139280546644, 0.9831962682546052, 0.9871254840793293, 0.99109023543049, 0.9947225483602016, 0.9977899266945391, 1.0001774990066643, 1.001864668350672, 1.0028999476902742, 1, 1);
+}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <div class="h-full flex-1 bg-white m-2">
     <!-- Populate a lot of content -->
     <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://svelte.dev/docs/kit">s
-velte.dev/docs/kit</a> to read the documentation</p>
-
-
-    <p>Here you can add more content to test the layout and responsiveness of your application.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </div>
